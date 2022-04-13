1060368184e50dff6f7d091a55877c40bde94143
Owen <gamingsars@gmail.com>
2022-04-13 23:51:15 +0100

    GUI Update & Added Sorting + Allowing to search for sentences

--- src/test/GUI.java
+++ src/test/GUI.java
@@ -1,36 +1,31 @@
 package test;
 
-
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.List;
 import javax.swing.filechooser.*;
-
-
-
 
 public class GUI implements ActionListener {
     JFileChooser jFileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-    JButton btn1 = new JButton("Submit");
-    JButton btn2 = new JButton("Open");
-    JButton btn3 = new JButton("Clear");
+    JButton btn1 = new JButton("SUBMIT");
+    JButton btn2 = new JButton("OPEN");
+    JButton btn3 = new JButton("CLEAR");
     JTextField textField = new JTextField();
-    JFrame frame;
-    JCheckBox checkBox1;
-    JCheckBox checkBox2;
-    JCheckBox checkBox3;
-    JCheckBox checkBox4;
+    JPanel panel = new JPanel();
+    List<JCheckBox> checkBoxes = new ArrayList<>();
 
+    SearchFiles sf;
+    JFrame frame;
     JLabel jLabel;
-    JTextArea jTextArea;
     String path;
     JScrollPane scroll;
+    JTextArea jTextArea;
 
     public GUI() {
         run();
@@ -38,7 +33,6 @@ public class GUI implements ActionListener {
 
    public void run() {
        // TODO: make icon work, and in the future it must be in the resources folder.
-       Icon icon = new ImageIcon("C:\\Users\\gamin\\SearchEngine\\src\\test\\icons");
 
        JFrame frame = new JFrame("Search Engine");
        frame.setVisible(true);
@@ -46,64 +40,52 @@ public class GUI implements ActionListener {
        frame.setResizable(false);
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       JPanel panel1 = new JPanel();
-       panel1.setBackground(Color.WHITE);
-       frame.add(panel1);
+       panel.setBackground(new Color(41, 50, 65));
+       frame.add(panel);
 
        JLabel lb = new JLabel("Type in a word: ");
        lb.setBackground(Color.yellow);
 
        textField.setBounds(355, 0, 2000, 30);
-      // textField.getMaximumSize(new Dimension(100,50));
-       //textField.setText();
-       textField.setPreferredSize(new Dimension(100,20));
-       panel1.add(textField);
+       textField.setPreferredSize(new Dimension(100,25));
+       textField.setBackground(new Color(91, 157, 194));
+       textField.setBorder(null);
+
+       panel.add(textField);
+
+       btn1.setBackground(new Color(224, 251, 252));
+       btn2.setBackground(new Color(224, 251, 252));
+       btn3.setBackground(new Color(224, 251, 252));
+
+       btn1.setOpaque(true);
+       btn2.setOpaque(true);
+       btn3.setOpaque(true);
 
        btn1.addActionListener(this);
        btn2.addActionListener(this);
        btn3.addActionListener(this);
-       btn1.setIcon(icon);
 
        jLabel = new JLabel("No file selected");
+       jLabel.setForeground(Color.WHITE);
 
-       panel1.add(btn1);
-       panel1.add(btn3);
-       panel1.add(btn2);
-       panel1.add(jLabel);
-
-
+       panel.add(btn1);
+       panel.add(btn3);
+       panel.add(btn2);
+       panel.add(jLabel);
 
        jTextArea = new JTextArea(25,30);
        jTextArea.setVisible(true);
        jTextArea.setLocation(30,110);
-       jTextArea.setSize(270,200);
-       jTextArea.setBackground(new Color(255,255,240));
+       jTextArea.setSize(270,210);
+       jTextArea.setBackground(new Color(91, 157, 194));
+       jTextArea.setBorder(null);
        jTextArea.setLineWrap(true);
        jTextArea.setWrapStyleWord(true);
+
        scroll = new JScrollPane(jTextArea);
-       //panel1.add(jTextArea);
-       panel1.add(scroll);
-
-       checkBox1 = new JCheckBox("File 1");
-       checkBox1.setBounds(100,100, 50,50);
-       checkBox2 = new JCheckBox("File 2");
-       checkBox2.setBounds(100,100, 50,50);
-       checkBox3 = new JCheckBox("File 3");
-       checkBox3.setBounds(100,100, 50,50);
-       checkBox4 = new JCheckBox("File 4");
-       checkBox4.setBounds(100,100, 50,50);
-
-       panel1.add(checkBox1);
-       panel1.add(checkBox2);
-       panel1.add(checkBox3);
-       panel1.add(checkBox4);
-
+       panel.add(scroll);
 
        frame.setVisible(true);
-
-
-
-
     }
 
    /* public void initialiseSpellChecker(){
@@ -117,45 +99,82 @@ public class GUI implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == btn1) {
-            if(textField.getText().trim().isEmpty()){
+            if (textField.getText().trim().isEmpty()) {
                 JOptionPane.showMessageDialog(frame, "Please enter a search query");
                 return;
             }
-            if(path == null){
+
+            if (path == null) {
                 JOptionPane.showMessageDialog(frame, "Please select a path");
                 return;
             }
-            SearchFiles sf;
-            sf = new SearchFiles(path);
+
             try {
-                HashMap<String, Integer> occurrences = sf.Search(textField.getText());
+                Map<String, Integer> occurrences = sf.Search(textField.getText());
                 String s = String.format("\nThe word %s appears in the following:\n", textField.getText());
                 jTextArea.append(s);
 
-                occurrences.forEach((key, value) -> {
-                    String formattedString = String.format("%s - %ox\n", key, value);
-                    jTextArea.append(formattedString);
-                });
+                int index = 0;
 
+                for (Map.Entry<String, Integer> set : occurrences.entrySet()) {
+                    String key = set.getKey();
+                    Integer value = set.getValue();
+
+                    String formattedString = switch (index) {
+                        case 0 -> String.format("Strongest link %s - %ox\n", key, value);
+                        case 1 -> String.format("Second strongest link %s - %ox\n", key, value);
+                        default -> String.format("%s - %ox\n", key, value);
+                    };
+
+                    jTextArea.append(formattedString);
+                    index++;
+                }
             } catch (FileNotFoundException fileNotFoundException) {
                 JOptionPane.showMessageDialog(frame, fileNotFoundException.getMessage());
             }
 
-            //jTextArea.setVisible(true);
         } else if (e.getSource() == btn2) {
             jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int r = jFileChooser.showSaveDialog(null);
+            int r = jFileChooser.showOpenDialog(null);
 
             if (r == JFileChooser.APPROVE_OPTION) {
-                // set the label to the path of the selected directory
                 String selectedPath = jFileChooser.getSelectedFile().getAbsolutePath();
                 jLabel.setText(selectedPath);
+                sf = new SearchFiles(selectedPath);
                 path = selectedPath;
 
+                if (!checkBoxes.isEmpty()) {
+                    panel.removeAll();
+                    checkBoxes.clear();
+                }
+
+                File fileObj = new File(path);
+                File[] files = fileObj.listFiles((dir, name) -> name.endsWith(".txt"));
+
+                if (files != null) {
+                    for (File file : files) {
+                        JCheckBox checkBox = new JCheckBox(file.getName());
+                        checkBox.setBounds(100,100, 50,50);
+                        checkBox.addActionListener(this);
+
+                        panel.add(checkBox);
+                    }
+                }
+            }
+        } else if(e.getSource() == btn3) {
+            textField.setText("");
+            jTextArea.setText("");
+        }
+
+        if (e.getSource().getClass() == JCheckBox.class) {
+            JCheckBox checkBox = (JCheckBox)e.getSource();
+
+            if (sf.allowedFiles.contains(checkBox.getText())) {
+                sf.allowedFiles.remove(checkBox.getText());
                 return;
             }
-        } else if(e.getSource() == btn3){
-            textField.setText("");
+
+            sf.allowedFiles.add(checkBox.getText());
         }
     }
 }
--- src/test/SearchFiles.java
+++ src/test/SearchFiles.java
@@ -1,20 +1,29 @@
 package test;
 
 import java.io.*;
-import java.util.HashMap;
+import java.util.*;
+import java.util.regex.*;
+import java.util.stream.Collectors;
 
 public class SearchFiles {
     File f1;
+    File[] files;
+    List<String> allowedFiles;
 
     public SearchFiles(String path) {
         f1 = new File(path);
+        files = f1.listFiles((dir, name) -> name.endsWith(".txt"));
+        allowedFiles = new ArrayList<>();
     }
 
-    public HashMap<String, Integer> Search(String searchQuery) throws FileNotFoundException {
+    public Map<String, Integer> Search(String searchQuery) throws FileNotFoundException {
         HashMap<String, Integer> fileOccurrences = new HashMap<>();
-        File[] files = f1.listFiles((dir, name) -> name.endsWith(".txt"));
 
-        for(File file : files) {
+        for (File file : files) {
+            if (!allowedFiles.contains(file.getName())) {
+                continue;
+            }
+
             int occurrences = 0;
             BufferedReader inputStream = new BufferedReader(new FileReader(file));
 
@@ -22,15 +31,22 @@ public class SearchFiles {
                 String line;
 
                 while ((line = inputStream.readLine()) != null) {
-                    String[] splittedLine = line.split(" ");
+                    String[] splittedSearchQuery = searchQuery.split(" ");
 
-                    for (String word : splittedLine) {
-                        String[] splittedQuery = searchQuery.split(" ");
+                    if (splittedSearchQuery.length == 1) {
+                        String[] splittedLine = line.split(" ");
 
-                        for (String queryWord : splittedQuery) {
-                            if (word.equalsIgnoreCase(queryWord)) {
+                        for (String word : splittedLine) {
+                            if (word.equalsIgnoreCase(searchQuery)) {
                                 occurrences++;
                             }
+                        }
+                    } else {
+                        Pattern pattern = Pattern.compile(searchQuery);
+                        Matcher matcher = pattern.matcher(line);
+
+                        while (matcher.find()) {
+                            occurrences++;
                         }
                     }
                 }
@@ -41,6 +57,9 @@ public class SearchFiles {
             }
         }
 
-        return fileOccurrences;
+        return fileOccurrences.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (k, v) -> k, LinkedHashMap::new));
     }
 }
