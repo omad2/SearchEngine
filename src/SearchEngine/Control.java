6b1c730b35c768b529078224cf7f9ea9d31fdfd8
Ostray <72156064+sysOwen@users.noreply.github.com>
2022-04-15 16:23:51 +0100

    Update Control.java

--- src/SearchEngine/Control.java
+++ src/SearchEngine/Control.java
@@ -1,9 +1,3 @@
-/*Author: Owen Ramkurrun
- *Project: This project is a simple search engine that will
- *search through a folder of text files with the words or sentence you enter.
- *It will then show which text file has the highest link. It will display from
- *highest to lowest.
- */
 package SearchEngine;
 
 public class Control {
